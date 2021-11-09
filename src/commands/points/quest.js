68b9fe3a1c0bb0ef058bcb2e8f045bcaaab017ee
SprityON <71904569+SprityON@users.noreply.github.com>
2021-11-09 22:49:15 +0100

    small fix

---
+++ src/commands/points/quest.js
@@ -0,0 +1,20 @@
+const Utils = require('../../classes/utilities/Utils')
+
+module.exports = {
+  name: Utils.getCmdName(__filename, __dirname),
+  category: Utils.getCmdCategory(__filename),
+  usage: '',
+  aliases: [],
+  permissions: ['SEND_MESSAGES'],
+  timeout: 1000,
+
+  execute(msg, args) {
+    // code
+  },
+
+  help: {
+    enabled: false,
+    title: '',
+    description: ``,
+  }
+}
